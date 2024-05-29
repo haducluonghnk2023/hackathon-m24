330c74a8d06efef940807d376e138aeee0dc4e03
haducluonghnk2023 <haducluongdz@gmail.com>
2024-05-29 12:00:48 +0700

    add

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import CardDetail from "./CardDetail";
 import AddToCartNotificationProps from "./AddToCard";
 interface AddToCartNotificationProps {
